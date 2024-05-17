957e2f4fe06ab9914097dbbffb82686006937f0b
Ronei Costa Soares <ronei0612@gmail.com>
2024-05-17 10:50:50 -0300

    Conversor json

---
+++ Migração/Conversor.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migração
+{
+	internal class Conversor
+	{
+		public void ConverterJsonExcel(string json, string caminhoArquivoExcel)
+		{
+			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+			json = File.ReadAllText(json);
+			// Converte o JSON em uma lista de dicionários
+			var dados = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(json);
+
+			// Cria um DataTable a partir dos dados do JSON
+			var dataTable = new DataTable();
+			foreach (var key in dados[0].Keys)
+			{
+				dataTable.Columns.Add(key);
+			}
+			foreach (var item in dados)
+			{
+				try
+				{
+					var row = dataTable.NewRow();
+					foreach (var key in item.Keys)
+					{
+						row[key] = item[key];
+					}
+					dataTable.Rows.Add(row);
+				}
+				catch { }
+			}
+
+			// Cria um arquivo Excel a partir do DataTable
+			using (var package = new ExcelPackage())
+			{
+				var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+				worksheet.Cells["A1"].LoadFromDataTable(dataTable, true);
+				package.SaveAs(new FileInfo(caminhoArquivoExcel));
+			}
+		}
+	}
+}

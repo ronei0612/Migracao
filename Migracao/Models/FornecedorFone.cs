603cea62e8c0bc1f104ef3662e73cbb48cd8713a
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-05-20 20:19:32 -0300

    Nova classe

---
+++ Migracao/Models/FornecedorFone.cs
@@ -0,0 +1,15 @@
+﻿namespace Migracao.Models
+{
+	internal class FornecedorFone
+	{
+		public int ID { get; set; }
+		public required int FornecedorID { get; set; }
+		public required short FoneTipoID { get; set; }
+		public required long Telefone { get; set; }
+		public string Extensao { get; set; }
+		public string ContatoNome { get; set; }
+		public int LoginID { get; set; }
+		public required DateTime DataInclusao { get; set; }
+		public DateTime DataUltAlteracao { get; set; }
+	}
+}

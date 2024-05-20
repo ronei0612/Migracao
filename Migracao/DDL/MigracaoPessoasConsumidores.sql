9abbc2dd4bf040644fcd35e19eeef5383a662eeb
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-05-20 18:20:18 -0300

    Testando

--- Migracao/DDL/MigracaoPessoasConsumidores.sql
+++ Migracao/DDL/MigracaoPessoasConsumidores.sql
@@ -17,32 +17,57 @@ BEGIN
     -- Pegar o primeiro ID inserido
     SELECT @PrimeiroID = MIN(PessoaID) FROM @IDs;
  
-   -- Atualizar a tabela _MigracaoConsumidores_Temp com os números em ordem crescente a partir do primeiro ID gerado
+    -- Atualizar a tabela _MigracaoConsumidores_Temp com os números em ordem crescente a partir do primeiro ID gerado
     DECLARE @i INT = 0;
-    WHILE @i < (SELECT COUNT(*) FROM _MigracaoConsumidores_Temp)
+    WHILE @i < (SELECT COUNT(*) FROM [_MigracaoConsumidores_Temp])
     BEGIN
 
         UPDATE [_MigracaoPessoaFones_Temp]
         SET PessoaID = @PrimeiroID + @i 
-        WHERE PessoaID = (SELECT PessoaID FROM _MigracaoConsumidores_Temp WHERE ID = @i + 1);
+        WHERE PessoaID = (SELECT PessoaID FROM [_MigracaoConsumidores_Temp] WHERE ID = @i + 1);
 	    
-        UPDATE _MigracaoConsumidores_Temp
+        UPDATE [_MigracaoConsumidores_Temp]
         SET PessoaID = @PrimeiroID + @i
         WHERE ID = @i + 1;
  
         SET @i = @i + 1;
     END
- 
-    SET NOCOUNT OFF;
-   
-   
-   -- Inserção dos dados na tabela Pessoas e armazenamento dos IDs inseridos
-    INSERT INTO Consumidores (Ativo, DataInclusao, EstabelecimentoID, LGPDSituacaoID, LoginID, PessoaID, CodigoAntigo)
-    SELECT Ativo, DataInclusao, EstabelecimentoID, LGPDSituacaoID, LoginID, PessoaID, CodigoAntigo
-    FROM [_MigracaoConsumidores_Temp];
    
    -- Inserção dos dados na tabela Pessoas e armazenamento dos IDs inseridos
     INSERT INTO PessoaFones (PessoaID, FoneTipoID, Telefone, DataInclusao, LoginID)
     SELECT PessoaID, FoneTipoID, Telefone, DataInclusao, LoginID
     FROM [_MigracaoPessoaFones_Temp];
+   
+   
+   -- Variável para armazenar o ID da pessoa inserida
+    DECLARE @PrimeiroConsudmidorID INT;
+    -- Tabela temporária para armazenar os IDs das pessoas inseridas
+    DECLARE @IDConsumidores TABLE (ID INT IDENTITY(1,1), ConsumidorID INT);
+   
+   -- Inserção dos dados na tabela Pessoas e armazenamento dos IDs inseridos
+    INSERT INTO Consumidores (Ativo, DataInclusao, EstabelecimentoID, LGPDSituacaoID, LoginID, PessoaID, CodigoAntigo)
+    OUTPUT INSERTED.ID INTO @IDConsumidores
+    SELECT Ativo, DataInclusao, EstabelecimentoID, LGPDSituacaoID, LoginID, PessoaID, CodigoAntigo
+    FROM [_MigracaoConsumidores_Temp];
+   
+   -- Pegar o primeiro ID inserido
+    SELECT @PrimeiroConsudmidorID = MIN(ConsumidorID) FROM @IDConsumidores;
+ 
+    -- Atualizar a tabela _MigracaoConsumidores_Temp com os números em ordem crescente a partir do primeiro ID gerado
+    DECLARE @j INT = 0;
+    WHILE @j < (SELECT COUNT(*) FROM [_MigracaoConsumidores_Temp])
+    BEGIN
+	    UPDATE [_MigracaoConsumidorEnderecos_Temp]
+        SET ConsumidorID = @PrimeiroConsudmidorID + @j
+        WHERE ConsumidorID = (SELECT ID FROM [_MigracaoConsumidores_Temp] WHERE ID = @j + 1);
+ 
+        SET @j = @j + 1;
+    END
+    
+    INSERT INTO ConsumidorEnderecos (Ativo, ConsumidorID, EnderecoTipoID, LogradouroTipoID, Logradouro, CidadeID, Cep, DataInclusao, Bairro, LogradouroNum, Complemento)
+    OUTPUT INSERTED.ID INTO @IDConsumidores
+    SELECT Ativo, ConsumidorID, EnderecoTipoID, LogradouroTipoID, Logradouro, CidadeID, Cep, DataInclusao, Bairro, LogradouroNum, Complemento
+    FROM [_MigracaoConsumidorEnderecos_Temp];
+ 
+    SET NOCOUNT OFF;
 END;

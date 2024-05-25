c6e864e6e8a72b4e7da282f6e05bdb1ff4f9e4cd
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-05-25 15:53:17 -0300

    Melhoria e correção

--- Migracao/Scripts/Export_Recebiveis.sql
+++ Migracao/Scripts/Export_Recebiveis.sql
@@ -3,4 +3,8 @@ DECLARE @EstabelecimentoID INT = 999999999;
 SELECT
     *
 FROM Recebiveis r
-WHERE r.EstabelecimentoID = @EstabelecimentoID;
+LEFT JOIN FluxoCaixa fc ON fc.RecebivelID = r.ID
+WHERE fc.pagovalor IS NULL
+AND r.databaixa IS NULL
+AND r.ExclusaoData IS NULL
+AND r.EstabelecimentoID = @EstabelecimentoID;

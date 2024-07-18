f2f1134d8f67ecfac05c0bc8fb38281635a57963
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-07-18 20:19:02 -0300

    Correção

--- Migracao/Models/DTO/ProcedimentosManutencaoDTO.cs
+++ Migracao/Models/DTO/ProcedimentosManutencaoDTO.cs
@@ -40,11 +40,11 @@ namespace Migracao.Models.DTO
         [DisplayName("Procedimento Nome")]
         public string? Procedimento_Nome { get; set; }
 
-        [DisplayName("Procedimento Valor")]
+        [DisplayName("Valor")]
         [DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = true)]
         public string? Procedimento_Valor { get; set; }
 
-        [DisplayName("Procedimento Observação")]
+        [DisplayName("Observação")]
         public string? Procedimento_Observacao { get; set; }
 
         [DisplayName("Data Hora Início")]

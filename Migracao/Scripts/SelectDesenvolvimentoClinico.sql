7bcb0b7138147fd28146557604d12a40cc389a23
Ronei Costa Soares <ronei.costa@aplicativo.net>
2024-07-24 14:56:59 -0300

    Correção

--- Migracao/Scripts/SelectDesenvolvimentoClinico.sql
+++ Migracao/Scripts/SelectDesenvolvimentoClinico.sql
@@ -1,17 +1,3 @@
---SELECT
---man101.LANCTO Lancamento,
---man101.CNPJ_CPF Paciente_CPF,
---emd101.NOME Paciente_Nome,
---man101.DATA_RETORNO Data_Retorno,
---man101.RESP_ATEND Dentista_Codigo,
---man101.NOME_RESP_ATEND Dentista_Nome,
---man101.TIPO_ATEND Procedimento_Nome,
---man101.DATA_LANC Data_Inicio,
---man101.DATA_RETORNO Data_Retorno,
---man101.OBS_ATEND Procedimento_Observacao
---FROM MAN101 man101 
---INNER JOIN EMD101 emd101 ON emd101.CGC_CPF = man101.CNPJ_CPF
-
 SELECT
 man111.CNPJ_CPF Paciente_CPF,
 emd101.NOME Paciente_Nome,
@@ -22,6 +8,7 @@ FROM MAN111 man111
 LEFT JOIN EMD101 emd101 ON emd101.CGC_CPF = man111.CNPJ_CPF
 LEFT JOIN MAN101 man101 ON (man101.CNPJ_CPF = man111.CNPJ_CPF AND man101.LANCTO = man111.LANCTO)
 WHERE man101.TIPO_ATEND IS NULL AND man111.NOME_TIPO IS NULL
+AND man111.DATA_ATEND IS NOT NULL
 UNION 
 SELECT
 anotacao_clinica.CNPJ_CPF Paciente_CPF,
